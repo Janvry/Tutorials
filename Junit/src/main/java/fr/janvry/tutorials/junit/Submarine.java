ade2ef021fb2c7a3675f543c9db46e339d2bddd8
Yoann Siméoni <yoann.simeoni@gmail.com>
2014-10-23 16:46:46 +0200

    Add Submarine class and test.

---
+++ Junit/src/main/java/fr/janvry/tutorials/junit/Submarine.java
@@ -0,0 +1,15 @@
+package fr.janvry.tutorials.junit;
+
+public class Submarine {
+
+	private int depth;
+
+	public int getDepth() {
+		return depth;
+	}
+
+	public void setDepth(int depth) {
+		this.depth = depth;
+	}
+
+}
---
+++ Junit/src/test/java/fr/janvry/tutorials/junit/SubmarineTest.java
@@ -0,0 +1,19 @@
+package fr.janvry.tutorials.junit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SubmarineTest {
+
+	@Test
+	public void testGetDepth() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetDepth() {
+		fail("Not yet implemented");
+	}
+
+}
